680c1fca640f289cb49b4d8bfa22e10a76c9e885
Nerio Balza <neriojbd@gmail.com>
2021-05-07 17:18:32 -0400

    Creando el router con react-router

---
+++ src/pages/NotesEdit.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotesEdit = () => {
+  return <div></div>;
+};
+
+export default NotesEdit;
--- src/router/App.js
+++ src/router/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import Layout from "../components/Layout";
+
+import Home from "../pages/Home";
+import Notes from "../pages/Notes";
+import NotesEdit from "../pages/NotesEdit";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/notes" component={Notes} />
+          <Route exact path="/note/:id/edit" component={NotesEdit} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
