24e3e4e2b667b56bdaee83dc469096593826f022
Nerio Balza <neriojbd@gmail.com>
2021-05-12 15:33:27 -0400

    Cambio de paleta de color y Componente de redes sociales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import SocialMedia from "./SocialMedia";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div>Navegacion por la pagina</div>
-      <div>Links de redes sociales</div>
+      <SocialMedia />
       <div>Links del Respositorio</div>
       <div>Derechos reservados.</div>
     </footer>
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SocialMedia = () => {
+  return (
+    <div className="social-media">
+      <a href="https:/google.com" target="blank">
+        <i className="icon-github social-media__icon"></i>
+      </a>
+      <a href="https:/google.com" target="blank">
+        <i className="icon-twitter social-media__icon"></i>
+      </a>
+      <a href="https:/google.com" target="blank">
+        <i className="icon-linkedin social-media__icon"></i>
+      </a>
+    </div>
+  );
+};
+
+export default SocialMedia;
