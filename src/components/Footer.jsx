5a9920612824f6f7adaa1cf388641dcaeccd1410
Nerio Balza <neriojbd@gmail.com>
2021-06-04 20:19:41 -0400

    Editando links del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,6 @@ const Footer = () => {
         <Link className="footer__link" to="/">
           Home
         </Link>
-        <Link className="footer__link" to="/about">
-          About
-        </Link>
       </div>
       <SocialMedia />
       <div>
