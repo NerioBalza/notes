66cdc81678a695d1825f5964c0198295c2fb8e65
Nerio Balza <neriojbd@gmail.com>
2021-08-18 19:45:57 -0400

    Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Layout from "../components/Layout";
 
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <Layout>
+      <main className="not-found">
+        <section className="not-found__container">
+          <div className="not-found__message">
+            <h2>Error 404:</h2>
+            <h3>Not Found</h3>
+          </div>
+          <Link className="not-found__link" to="/">
+            Go back to Home
+          </Link>
+        </section>
+      </main>
+    </Layout>
+  );
 };
 
 export default NotFound;
